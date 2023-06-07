8e066671eebc6384be53f744881462e52937f1be
zhujie <zhujie@WUDIGAMINGLAPTO>
2023-06-07 21:55:23 +0800

    implement chart demo

---
+++ RLP.Chart.Client/ChartTestWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using RLP.Chart.Interface;
+using RLP.Chart.OpenGL.Renderer;
+
+namespace RLP.Chart.Client
+{
+    /// <summary>
+    /// ChartTestWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class ChartTestWindow : Window
+    {
+        public ChartTestWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ChartTestWindow_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            var lineRenderer = LineChart.NewSeries();
+            LineChart.SettingRegion = new Region2D(new ScrollRange(0, 100), new ScrollRange(0, 100));
+            var random = new Random();
+            for (int i = 0; i < 100; i++)
+            {
+                lineRenderer.Add(new Point2D(i, 200 + i + random.Next(-100, 100)));
+            }
+        }
+    }
+}
---
+++ RLP.Chart.OpenGL/CollisionDetection/ICollisionPoint2D.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using RLP.Chart.Interface.Abstraction;
+using RLP.Chart.OpenGL.Renderer;
+
+namespace RLP.Chart.OpenGL.CollisionDetection
+{
+    /// <summary>
+    /// 二维点碰撞层，给每个类型的集合分配独立的碰撞检测层以最大化内存效率
+    /// </summary>
+    public interface ICollisionPoint2D : IGeometryCollection<IPoint2D>
+    {
+        /// <summary>
+        /// used for finding
+        /// </summary>
+        Guid Id { get; }
+
+        bool TrySearch(ICollisionGeometry2D geometry, out Node point);
+    }
+}
